6dfc0f74faa72c8b3190402ade47409bc46b231c
marlo <marlo@NB-IGOR>
2024-03-09 17:17:20 -0300

    connection provider class

---
+++ src/dao/ConnectionProvider.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+
+public class ConnectionProvider {
+    
+    public static Connection getCon(){
+        try{
+            Class.forName("com.sql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/")
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+}
