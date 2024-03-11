8285ac3592c77423f8d30060af19f0a9d6b20ad5
marlo <marlo@NB-IGOR>
2024-03-10 23:31:13 -0300

    inserting user query

--- src/dao/tables.java
+++ src/dao/tables.java
@@ -13,8 +13,9 @@ public class tables {
         try{
             con = ConnectionProvider.getCon();
             st = con.createStatement();
-            st.executeUpdate("CREATE TABLE user(user_id int AUTO_INCREMENT primary key, userType VARCHAR(50), name VARCHAR(200), password VARCHAR(50), status VARCHAR(50)) ");
-            
+            //st.executeUpdate("CREATE TABLE user(user_id int AUTO_INCREMENT primary key, userType VARCHAR(50), name VARCHAR(200), password VARCHAR(50), status VARCHAR(50)) ");
+            st.executeUpdate("INSERT INTO user(userType, name, password, status) VALUES ('SuperAdmin', 'Super Admin', 'admin', 'Active')");
+            JOptionPane.showMessageDialog(null, "Comando Executado com Sucesso!");
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
